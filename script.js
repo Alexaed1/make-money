eb5ac84295d3a71040435e64fdfdf92f44919241
Alexa Edwards <alexaedwards48@gmail.com>
2022-08-01 21:46:43 -0400

    initial commit

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 "use strict";
 
-let sum = 0;
-const totalParagraph = document.querySelector(".total");
-const buttons = document.querySelectorAll(".vending button");
-
-
-buttons.forEach((item) => {
-  item.addEventListener("click", () => {
-    sum += parseInt(item.getAttribute("data-cost"));
-    totalParagraph.textContent = `Total: $${sum}.00`;
-  });
-});
-
 
 const coinContainer = document.querySelector(".coin-container");
 const moneyForm = document.querySelector(".money-form");
@@ -29,31 +17,4 @@ moneyForm.addEventListener("submit", (e) => {
       newCoin.remove();
     });
   }
-});
-
-
-const bulb = document.querySelector(".bulb");
-const onBtn = document.querySelector(".onBtn");
-const offBtn = document.querySelector(".offBtn");
-const toggleBtn = document.querySelector(".toggleBtn");
-const endBtn = document.querySelector(".endBtn");
-
-onBtn.addEventListener("click", () => {
-  bulb.classList.add("on");
-});
-
-offBtn.addEventListener("click", () => {
-  bulb.classList.remove("on");
-});
-
-toggleBtn.addEventListener("click", () => {
-  bulb.classList.toggle("on");
-});
-
-endBtn.addEventListener("click", () => {
-  bulb.remove();
-  onBtn.disabled = true;
-  offBtn.disabled = true;
-  toggleBtn.disabled = true;
-  endBtn.disabled = true;
 });
